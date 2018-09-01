49ff94bfc6aaf51443a1b0cc99699987ee302710
aakulasaikiran <aakulasaikiran@gmail.com>
2018-09-01 17:09:10 +0530

    commit3

---
+++ PasswordEncodingExample1/src/main/java/com/lsn/domain/Authorities.java
@@ -0,0 +1,31 @@
+package com.lsn.domain;
+
+public class Authorities {
+	private int authoritiesId;
+	private String username;
+	private String authority;
+
+	public int getAuthoritiesId() {
+		return authoritiesId;
+	}
+
+	public void setAuthoritiesId(int authoritiesId) {
+		this.authoritiesId = authoritiesId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+}
